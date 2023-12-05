e739317525eb4fb83321aa82f828a8c61143420e
Tom Xavier <tomxavieronthego@gmail.com>
2023-12-06 02:41:29 +0530

    updated avatar url for messages

--- webchat/serializers.py
+++ webchat/serializers.py
@@ -1,7 +1,23 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Conversation, Message
-from server.serializers import UserSerializer
+from accounts.models import UserAccount
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserAccount
+        fields = ['id', 'username', 'display_name', 'avatar']
+
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if 'avatar' in data and data['avatar']:
+            data['avatar'] = data['avatar'].replace(
+                "https://www.webcord.site", "")
+       
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer()
