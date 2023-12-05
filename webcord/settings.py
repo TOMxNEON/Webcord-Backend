a06f7ec9500f1db21c8acd40b35084d2f0c735fd
Tom Xavier <tomxavieronthego@gmail.com>
2023-12-06 01:19:15 +0530

    updated settings

--- webcord/settings.py
+++ webcord/settings.py
@@ -156,8 +156,12 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_URL = 'media/'
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type
